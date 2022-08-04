58ade38df4a1524845d6d89d26558c94d988bc55
HuaHuang <459379028@qq.com>
2022-08-04 16:28:08 +0800

    master commit5

--- src/main/java/com/hh/GitTest.java
+++ src/main/java/com/hh/GitTest.java
@@ -4,7 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix2");
+        int a = 0;
     }
 }
